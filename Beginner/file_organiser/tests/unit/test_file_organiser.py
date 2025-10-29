b21dcbe658502261aa9e5c0f6c4c9b57b4d6ec48
Adam Charlton <ad5665@live.co.uk>
2025-10-29 09:42:28 +0000

    file-org-new-fetures: refactor pytests

--- Beginner/file_organiser/tests/unit/test_file_organiser.py
+++ Beginner/file_organiser/tests/unit/test_file_organiser.py
@@ -2,19 +2,41 @@
 
 import pytest
 
-from file_organiser import get_files, get_type, process_files
+from file_organiser import main, get_files, get_type, process_files
 
 
+# Test main with args
 def test_path():
-    amount = process_files("/mnt/c/Users/Adam/Desktop/adhd/").total()
+    amount = main(["/mnt/c/Users/Adam/Desktop/adhd/"]).total()
+    assert amount > 0
+
+
+def test_no_path():
+    amount = main().total()
     assert amount > 0
 
 
 def test_not_dir():
     with pytest.raises(NotADirectoryError, match="is not a directory"):
-        list(get_files("/tmp/file-org/text.txt"))
+        list(main(["/tmp/file-org/text.txt"]))
 
 
-def test_get_image():
+def test_with_file():
+    file_type = main(["--file", "/mnt/c/Users/Adam/Desktop/adhd/20250820_160657.jpg"])
+    assert file_type == "image/jpeg"
+
+
+# Test get_files
+
+
+# Test get_type
+def test_get_type_image():
     type = get_type("/mnt/c/Users/Adam/Desktop/adhd/20250820_160657.jpg")
     assert type == "image/jpeg"
+
+
+def test_get_type_exception():
+    result = get_type(
+        "home/acharlton/Documents/git/mine/python-projects/Beginner/file_organiser/.venv/lib64"
+    )
+    assert "Failed to generate type of file for" in result
